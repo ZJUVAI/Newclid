04faa5fffcb81e54dd318afedd08dd29dc735537
Tianxiang Xia <xia.tianxiang@huawei.com>
2024-08-19 13:14:35 +0200

    Simplify the simbols graph model

--- src/geosolver/dependency/symbols_graph.py
+++ src/geosolver/dependency/symbols_graph.py
@@ -14,7 +14,7 @@ CircL = TypeVar("CircL", "Circle", "Line")
 
 class SymbolsGraph:
     def __init__(self) -> None:
-        self.type2nodes: dict[Type[Symbol], list[Symbol]] = {
+        self._type2nodes: dict[Type[Symbol], list[Symbol]] = {
             Point: list(),
             Line: list(),
             Circle: list(),
@@ -22,7 +22,7 @@ class SymbolsGraph:
         self.name2node: dict[str, Symbol] = {}
 
     def nodes_of_type(self, t: Type[S]) -> list[S]:
-        return list(self.type2nodes[t])  # type: ignore
+        return list(self._type2nodes[t])  # type: ignore
 
     def names2points(
         self, pnames: Iterable[str], create_new_point: bool = True
@@ -47,7 +47,7 @@ class SymbolsGraph:
         if name in self.name2node:
             raise ValueError(f"Node {name} already present")
         node = oftype(name, self, dep)
-        self.type2nodes[type(node)].append(node)
+        self._type2nodes[type(node)].append(node)
         self.name2node[node.name] = node
         return node
 
@@ -61,18 +61,12 @@ class SymbolsGraph:
     def line_thru_pair(self, p1: Point, p2: Point, table: Table) -> Line:
         for line in self.nodes_of_type(Line):
             if {p1, p2} <= line.points:
-                init_line: Optional[Line] = None
                 for line1 in line.fellows:
-                    if {p1, p2} <= line1.points and (
-                        init_line is None or len(init_line.points) > len(line1.points)
-                    ):
-                        init_line = line1
-                assert init_line is not None
-                if len(init_line.points) == 2:
-                    return init_line
+                    if {p1, p2} == line1.points:
+                        return line1
                 res = self._get_new_line_thru_pair(p1, p2)
-                assert init_line.dep
-                table.add_expr(table.get_eq2(res.name, init_line.name), init_line.dep)
+                assert line.dep
+                table.add_expr(table.get_eq2(res.name, line.name), line.dep)
                 line.fellows.append(res)
                 return res
         return self._get_new_line_thru_pair(p1, p2)
