47c3da31fe6be58aa274851d1ff79b6438351a13
zhengtongdu <zhengtongdu@zju.edu.cn>
2025-04-24 13:39:14 +0000

    add goal&proof Filter

--- src/newclid/generate/generate.sh
+++ src/newclid/generate/generate.sh
@@ -4,7 +4,7 @@
 # export PYTHONPATH=$PYTHONPATH:$MELIAD_PATH
 
 # start_time=$(date +%s)
-python generate.py --max_clauses=1 --search_depth=4 --n_threads=1 --n_samples=1 --log_level=info
+python generate.py --max_clauses=1 --search_depth=4 --n_threads=1 --n_samples=1 --log_level=info --min_dep_num=10
 # python analyze.py 
 # end_time=$(date +%s)
 # execution_time=$((end_time - start_time))
