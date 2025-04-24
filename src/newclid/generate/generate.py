3a75a2061075db7003477085a813f59a42bbdee5
MinfengZhu <minfeng_zhu@zju.edu.cn>
2025-04-24 05:50:02 +0000

    refactor generate.py

--- src/newclid/generate/generate.py
+++ src/newclid/generate/generate.py
@@ -12,7 +12,7 @@ from newclid.api import GeometricSolverBuilder
 from newclid.configs import default_defs_path, default_rules_path
 from newclid.generate.clause_generation import CompoundClauseGen
 from newclid.proof_writing import return_proof_steps
-
+from newclid.formulations.problem import ProblemJGEX
 
 class GeometryGenerator:
     def __init__(self, max_clauses=5, search_depth=5, n_threads=1, output_dir="dataset"):
@@ -20,7 +20,7 @@ class GeometryGenerator:
         self.search_depth = search_depth
         self.n_threads = n_threads
         self.output_dir = output_dir
-        
+
         self.clauses_generator = CompoundClauseGen(
             max_comma_sep_clause=2,
             max_single_clause=1,
@@ -41,7 +41,8 @@ class GeometryGenerator:
                 "fl_problem",
                 "nl_problem",
                 "nl_solution",
-                "dsl",
+                "fl_problem_renamed",
+                "dsl_problem_renamed"
             ]
             writer = csv.DictWriter(
                 csvfile, fieldnames=field_names, quoting=csv.QUOTE_MINIMAL, quotechar='"'
@@ -59,7 +60,7 @@ class GeometryGenerator:
             encoding="utf-8"
         ) as out_f:
             for i, row in enumerate(all_data):
-                out_f.write(f"{i}\n{row['fl_problem']}\n")
+                out_f.write(f"{i}\n{row['fl_problem_renamed']}\n")
 
         # Write to dsl.txt
         with open(
@@ -68,14 +69,13 @@ class GeometryGenerator:
             encoding="utf-8",
         ) as out_f:
             for row in all_data:
-                out_f.write(row["dsl"] + "\n")
+                out_f.write(row["dsl_problem_renamed"] + "\n")
 
         return len(all_data)
 
     def process_single_problem(self, args: tuple) -> list:
         """Process a single geometry problem."""
         pid, fl_statement, search_depth = args
-        random.seed(pid)
 
         solver_builder = GeometricSolverBuilder(seed=998244353)
         solver_builder.load_defs_from_file(default_defs_path())
@@ -84,7 +84,7 @@ class GeometryGenerator:
         solver_builder.load_problem_from_txt(fl_statement)
         
         try:
-            solver = solver_builder.build()
+            solver = solver_builder.build(max_attempts=100)
         except Exception as e:
             logging.info(f"Error: {e}")
             return []
@@ -94,39 +94,28 @@ class GeometryGenerator:
 
         generated_data = []
         for goal in goals:
-            solver.proof.goals = [goal]
+            # fl_problem
             essential_clauses, essential_aux_clauses = solver.proof.dep_graph.get_essential_clauses([goal])
+            n_clauses = len(essential_clauses) + len(essential_aux_clauses)
             statements = []
             for clause in solver_builder.problemJGEX.constructions:
                 if str(clause) in essential_clauses or str(clause) in essential_aux_clauses:
                     statements.append(str(clause))
-            shaved_problem = '; '.join(statements) + ' ? ' + goal.predicate.NAME + ' ' + ' '.join([arg.name for arg in goal.args])
-
-            solver_builder_shaved = GeometricSolverBuilder(seed=998244353)
-            solver_builder_shaved.load_defs_from_file(default_defs_path())
-            solver_builder_shaved.load_rules_from_file(default_rules_path())
-            solver_builder_shaved.with_deductive_agent(DDARN())
-            solver_builder_shaved.load_problem_from_txt(shaved_problem)
-            
-            try:
-                solver_shaved = solver_builder_shaved.build()
-            except Exception as e:
-                logging.info(f"Error: {e}")
-                continue
-
-            success = solver_shaved.run()
-            if success is False:
-                logging.info(f"Cannot shave the problem {shaved_problem}")
-                continue
-            else:
-                nl_solution = return_proof_steps(solver_shaved.proof)
-                generated_data.append({
-                    "n_clauses": len(solver_builder.problemJGEX.constructions),
-                    "fl_problem": shaved_problem,
-                    "nl_problem": "",
-                    "nl_solution": nl_solution,
-                    "dsl": "",
-                })
+            fl_problem = '; '.join(statements) + ' ? ' + goal.predicate.NAME + ' ' + ' '.join([arg.name for arg in goal.args])
+            # nl_solution
+            solver.proof.goals = [goal]
+            nl_solution = return_proof_steps(solver.proof)
+            # rename problem
+            renamed_problem = ProblemJGEX.from_text(fl_problem).renamed()
+            # output
+            generated_data.append({
+                "n_clauses": n_clauses,
+                "fl_problem": fl_problem,
+                "nl_problem": "",
+                "nl_solution": nl_solution,
+                "fl_problem_renamed": str(renamed_problem),
+                "dsl_problem_renamed": "",
+            })
 
         return generated_data
 
@@ -172,8 +161,7 @@ def main():
     )
     args = parser.parse_args()
 
-    log_level = getattr(logging, args.log_level.upper())
-    logging.basicConfig(level=log_level)
+    logging.basicConfig(level=getattr(logging, args.log_level.upper()))
 
     generator = GeometryGenerator(
         max_clauses=args.max_clauses,
@@ -185,7 +173,6 @@ def main():
     # Collect problems using the generator
     all_data = []
     start_time = time.time()
-    
     for problem in generator.generate_problems():
         all_data.append(problem)
         logging.info(
