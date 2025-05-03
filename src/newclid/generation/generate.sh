78d5dd1e73ad1720ff541a39470198a756ca3011
MinfengZhu <minfeng_zhu@zju.edu.cn>
2025-05-03 12:38:58 +0000

    fix bugs in proof_filter, dsl points name; refactor generate_problems func

--- src/newclid/generation/generate.sh
+++ src/newclid/generation/generate.sh
@@ -4,8 +4,8 @@
 # export PYTHONPATH=$PYTHONPATH:$MELIAD_PATH
 
 # start_time=$(date +%s)
-python generate.py --max_clauses=10 --search_depth=1 --n_threads=1 --n_samples=10 --log_level=info
-# python -m cProfile -o dataset/profile.prof -s cumulative -m generate --max_clauses=4 --search_depth=9 --n_threads=1 --n_samples=100 --log_level=info
+python generate.py --max_clauses=10 --search_depth=10 --n_threads=1 --n_samples=100 --log_level=info
+# python -m cProfile -o dataset/profile.prof -s cumulative -m generate --max_clauses=4 --search_depth=9 --n_threads=1 --n_samples=1 --log_level=info
 
 # python analyze.py 
 # end_time=$(date +%s)
