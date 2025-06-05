78f48c4dff58ff1fc929db359cd6624185cdba60
MinfengZhu <minfeng_zhu@zju.edu.cn>
2025-06-05 14:04:06 +0000

    refactor: remove search_depth/max_level; add agent evaluation; change output to json format

--- src/newclid/proof_writing.py
+++ src/newclid/proof_writing.py
@@ -33,14 +33,14 @@ def get_structured_proof(proof_state: "ProofState", id: dict[Statement, str]) ->
     points = sorted([p.pretty_name for p in points])
     aux_points = sorted([p.pretty_name for p in aux_points])
 
-    analysis = "<analysis>\n"
+    analysis = "<analysis> "
     for line in premises:
         if line.statement not in id:
             id[line.statement] = f"{len(id):03d}"
     sorted_premises = sorted(premises, key=lambda line: id[line.statement])
     for line in sorted_premises:
-        analysis += pure_predicate(line) + "\n"
-    analysis += "</analysis>\n"
+        analysis += pure_predicate(line) + " "
+    analysis += "</analysis>"
 
     numerical_check = ""
     for line in numercial_checked_premises:
@@ -48,93 +48,26 @@ def get_structured_proof(proof_state: "ProofState", id: dict[Statement, str]) ->
             id[line.statement] = f"{len(id):03d}"
     sorted_numercial_checked_premises = sorted(numercial_checked_premises, key=lambda line: id[line.statement])
     for line in sorted_numercial_checked_premises:
-        numerical_check += pure_predicate(line) + "\n"
+        numerical_check += pure_predicate(line) + " "
     for line in numercial_checked_aux:
         if line.statement not in id:
             id[line.statement] = f"{len(id):03d}"
     sorted_numercial_checked_aux = sorted(numercial_checked_aux, key=lambda line: id[line.statement])
     for line in sorted_numercial_checked_aux:
-        numerical_check += pure_predicate(line) + "\n"
+        numerical_check += pure_predicate(line) + " "
     if len(numerical_check) > 0:
-        numerical_check = "<numerical_check>\n" + numerical_check + "</numerical_check>\n"
+        numerical_check = "<numerical_check> " + numerical_check + "</numerical_check>"
 
-    proof = "<proof>\n"
+    proof = "<proof> "
     for k, line in enumerate(proof_steps):
         if NUMERICAL_CHECK not in line.reason and IN_PREMISES not in line:
-            proof += f"{rediger(line)}\n"
-    proof += "</proof>\n"
+            proof += f"{rediger(line)} "
+    proof += "</proof>"
 
     return analysis, numerical_check, proof
 
 
-def return_proof_steps(proof_state: "ProofState") -> list[Dependency]:
-    id: dict[Statement, str] = {}
-    goals = [goal for goal in proof_state.goals if goal.check()]
-    for k, goal in enumerate(goals):
-        id[goal] = f"g{k}"
-
-    def rediger(dep: Dependency) -> str:
-        for statement in (dep.statement,) + dep.why:
-            if statement not in id:
-                id[statement] = str(len(id) - len(goals))
-        return f"{', '.join(premise.pretty() + ' [' + id[premise] + ']' for premise in dep.why)} ({dep.reason})=> {dep.statement.pretty()} [{id[dep.statement]}]"
-
-    # solution = "==========================\n"
-    # solution += "* From problem construction:\n"
-    # solution += f"Points : {', '.join(p.pretty_name for p in proof_state.symbols_graph.nodes_of_type(Point))}\n"
-    # proof_deps = proof_state.dep_graph.proof_deps(goals)
-    # premises: list[Dependency] = []
-    # numercial_checked: list[Dependency] = []
-    # proof_steps: list[Dependency] = []
-    # for line in proof_deps:
-    #     if IN_PREMISES == line.reason:
-    #         premises.append(line)
-    #     elif NUMERICAL_CHECK == line.reason:
-    #         numercial_checked.append(line)
-    #     else:
-    #         proof_steps.append(line)
-    # for line in premises:
-    #     solution += rediger(line) + "\n"
-    # for line in numercial_checked:
-    #     solution += rediger(line) + "\n"
-
-    (
-        points,
-        premises,
-        numercial_checked_premises,
-        aux_points,
-        aux,
-        numercial_checked_aux,
-        proof_steps,
-    ) = proof_state.dep_graph.get_proof_steps(goals)
-    points = sorted([p.pretty_name for p in points])
-    aux_points = sorted([p.pretty_name for p in aux_points])
-
-    solution = "==========================\n"
-    solution += "* From theorem premises:\n"
-    solution += f"Points : {', '.join(points)}\n"
-    for line in premises:
-        solution += rediger(line) + "\n"
-    for line in numercial_checked_premises:
-        solution += rediger(line) + "\n"
-
-    solution += "\n* Auxiliary Constructions:\n"
-    solution += f"Points : {', '.join(aux_points)}\n"
-    for line in aux:
-        solution += rediger(line) + "\n"
-    for line in numercial_checked_aux:
-        solution += rediger(line) + "\n"
-
-    solution += "\n* Proof steps:\n"
-    for k, line in enumerate(proof_steps):
-        if NUMERICAL_CHECK not in line.reason and IN_PREMISES not in line:
-            solution += f"{k:03d}. {rediger(line)}\n"
-    solution += "=========================="
-
-    return solution
-
-
-def write_proof_steps(proof_state: "ProofState", out_file: Optional[Path]) -> None:
+def write_proof_steps(proof_state: "ProofState", out_file: Optional[Path] = None, print_output: bool = True) -> None:
     """Output the solution to out_file.
 
     Args:
@@ -205,9 +138,9 @@ def write_proof_steps(proof_state: "ProofState", out_file: Optional[Path]) -> No
         if NUMERICAL_CHECK not in line.reason and IN_PREMISES not in line:
             solution += f"{k:03d}. {rediger(line)}\n"
     solution += "=========================="
-    if out_file is None:
+    if out_file is None and print_output is True:
         print(solution)
-    else:
+    elif out_file is not None:
         out_file.parent.mkdir(parents=True, exist_ok=True)
         with open(out_file, "w", encoding="utf-8") as f:
             f.write(solution)
