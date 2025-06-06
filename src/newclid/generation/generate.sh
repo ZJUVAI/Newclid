d924d119d7f78f435ff485fbc0f21c4d0b000781
MinfengZhu <minfeng_zhu@zju.edu.cn>
2025-06-06 10:15:48 +0000

    formate generation logging

--- src/newclid/generation/generate.sh
+++ src/newclid/generation/generate.sh
@@ -3,7 +3,7 @@
 # MELIAD_PATH=$(pwd)/../meliad_lib/meliad
 # export PYTHONPATH=$PYTHONPATH:$MELIAD_PATH
 
-python generate.py --max_clauses=10 --n_threads=30 --n_samples=10000 --log_level=info --timeout=7200
+python generate.py --max_clauses=15 --n_threads=30 --n_samples=1000000 --log_level=info --timeout=7200
 # python -m cProfile -o dataset/profile.prof -s cumulative -m generate --max_clauses=4 --search_depth=9 --n_threads=1 --n_samples=1 --log_level=info
 
 # python equiv_analyze.py geometry_depth${search_depth}_raw.csv dataset/output.txt
