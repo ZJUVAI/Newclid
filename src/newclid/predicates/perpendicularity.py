203c630c935812e24f20468b030f27e8c9087d90
MinfengZhu <minfeng_zhu@zju.edu.cn>
2025-10-09 15:24:04 +0000

    fix: some issue in statement.check when check numerical and ar. use try except to skip.

--- src/newclid/predicates/perpendicularity.py
+++ src/newclid/predicates/perpendicularity.py
@@ -72,7 +72,10 @@ class Perp(Predicate):
 
     @classmethod
     def check(cls, statement: Statement) -> bool:
-        eqs, table = cls._prep_ar(statement)
+        try:
+            eqs, table = cls._prep_ar(statement)
+        except Exception as e:
+            return False
         return all(table.expr_delta(eq) for eq in eqs)
 
     @classmethod
