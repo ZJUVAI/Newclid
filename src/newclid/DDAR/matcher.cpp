c5f49087a90c264ed30eea4e1d916819795a1bc5
zhengtong-cy <zhengtongdu@zju.edu.cn>
2026-04-12 02:40:31 +0800

    sync: remove DDAR_WEAK guard in on_cyclic (pre-sync local change)

--- src/newclid/DDAR/matcher.cpp
+++ src/newclid/DDAR/matcher.cpp
@@ -512,9 +512,7 @@ void Matcher::on_cyclic(const Cyclic &cyclic)
 {
     for (const auto &rotated : cyclic.permutation())
     {
-        #ifndef DDAR_WEAK
             insert_theorem(Theorem::cyclic_of_equal_angles(rotated));
-        #endif 
             insert_theorem(Theorem::cyclic_properties(cyclic));
     }
 }
