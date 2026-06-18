ff8f0a1bc546f9f610d8cc3e39b7c738fe1d0815
BruceW-07 <2399512924@qq.com>
2026-06-18 07:47:14 +0000

    test dsl basic order for incenter2

--- tests/test_search_runtime_dsl.py
+++ tests/test_search_runtime_dsl.py
@@ -18,3 +18,16 @@ def test_problem_dsl_uses_basic_order():
         dsl
         == "<problem> a : ; b : ; c : rconst a b a c 1/2 [000] ? cong a b a c </problem>"
     )
+
+    problem = ProblemJGEX.from_text(
+        "x y z i a b c = incenter2 x y z i a b c ? cong x y x y"
+    )
+    dsl = problem_to_dsl(problem, defs)
+
+    assert (
+        dsl
+        == "<problem> i : eqangle a b a i a i a c [000] eqangle a c c i c i b c [001] ; "
+        "x : coll b c x [002] perp b c i x [003] ; "
+        "y : coll a c y [004] perp a c i y [005] ; "
+        "z : coll a b z [006] perp a b i z [007] ? cong x y x y </problem>"
+    )
